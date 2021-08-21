2055eecee23eb3f1fa3f98ad872c1d5da627f4a4
bla2bla <bla2bla@users.noreply.github.com>
2021-08-21 01:48:31 +0100

    ok

--- hotel/iskandar/inscription.php
+++ hotel/iskandar/inscription.php
@@ -4,7 +4,12 @@
 <head>
 
 
-// skrrrt 
+// skrrrt
+
+//
+
+
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="">
